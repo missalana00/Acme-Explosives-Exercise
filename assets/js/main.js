75b6c41e71247f0808322133ff307b0db87eb1df
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-08-01 13:44:58 -0500

    Completed project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Material Select Initialization - unique to MDB
-$(document).ready(function() {
-  $('.mdb-select').material_select();
-});
+// $(document).ready(function() {
+//   $('.mdb-select').material_select();
+// });
 
 
 //////////////////////////////
@@ -10,9 +10,11 @@ $(document).ready(function() {
 
 
 var output = $("#output");
-var ele1 = $("#categories");
-var ele2 = $("#types");
-var ele3 = $("#products");
+var categoryDD = $("#categories");
+var typesDD = $("#types");
+var productsDD = $("#products");
+var desciptDiv = $("#descriptionInput");
+var selectedId = null;
 
 
 //////////////////////////////
@@ -20,7 +22,7 @@ var ele3 = $("#products");
 ////////////////////////////
 
 
-//First load first XHR : Goes and gets categories JSON object and retrives the data // This one if fired immediately
+//First load first XHR : Goes and gets categories JSON object and retrives the data // This one is fired immediately
 var firstXHR = function() {
   return new Promise(function(resolve, reject) {
     $.ajax({
@@ -37,8 +39,9 @@ var firstXHR = function() {
 var secondXHR = function() {
   return new Promise(function(resolve, reject) {
     $.ajax({
-      url: "shared/json/types.json"      
+      url: "shared/json/types.json"
     }).done(function(data) {
+      // console.log("Data in function", data)
       resolve(data);
     }).fail(function(xhr, status, error) {
       reject(error);
@@ -50,10 +53,12 @@ var secondXHR = function() {
 var thirdXHR = function() {
   return new Promise(function(resolve, reject) {
     $.ajax({
-      url: "json/categories.json"
+      url: "shared/json/products.json"
     }).done(function(data) {
+      // console.log("data in function", data)
       resolve(data);
     }).fail(function(xhr, status, error) {
+      debugger;
       reject(error);
     });
   });
@@ -65,45 +70,119 @@ var thirdXHR = function() {
 ////////////////////////////
 
 // Dynamically populate DOM with data: Function creates a string to build DOM elements, 
-// loops through data and extracts categories from JSON, then adds an event listener that, upon click, fires 
-// the secondXHR function and then runs the populateDOMtypes function
+// loops through data and extracts categories from JSON, then adds an event listener that, upon change event,  
+// fires the secondXHR function and then runs the populateDOMtypes function
 var populateDOMcategories = function(data) {
-  console.log("data", data.categories);
 
- 
   var myString = '';
   
-  myString += `<select class="mdb-select colorful-select dropdown-primary">`;
-  myString += `<option disabled="true" selected="true" value="0">Select a categories</option>`;
+  myString += `<select class="primaryDD">`;
+  myString += `<option disabled="true" selected="true" value="0">Select a category</option>`;
   
       for (var i = 0; i < data.categories.length; i++) {
-        console.log(data.categories[i].name);
-        myString += `<option value="0">${data.categories[i].name}</option>`;
-      } 
+        myString += `<option value="${i}">${data.categories[i].name}</option>`;
+      }
 
   myString += `</select>`;
-  ele1.html(myString);
+  categoryDD.html(myString);
 
   // Event Listener
-  var categoryDD= $(".dropdown-primary")[0];
-  $(categoryDD).change(function(e) {
-    secondXHR().then(populateTypesDOM);
+  var mainDD= $(".primaryDD")[0];
+  $(mainDD).change(function(e) {
+    selectedId = e.target.value;
+    // secondXHR().then((data, e.target.value)=>populateDOMtypes); // If you use this, you would delete the global variable
+    secondXHR().then(populateDOMtypes);
   });
 };
 
 
-
-
-
+///// Function to dynamically populate DOM types
 var populateDOMtypes = function(data) {
-  console.log("data", data);
-  // dynamically populate dom with data
+  // console.log("data", data);
 
-  // Event Listeners
+  var myString = '';
+  
+  myString += `<select class="secondaryDD">`;
+  myString += `<option disabled="true" selected="true" value="0">Select a type</option>`;
+  
+      for (var i = 0; i < data.types.length; i++) {
+        // console.log("selectedId", selectedId)
+        // console.log("each data", data.types[i].category)
+
+        if (selectedId == data.types[i].category) {
+          myString += `<option value="${i}">${data.types[i].name}</option>`;
+        }
+      }
+
+  myString += `</select>`;
+  typesDD.html(myString);
+
+// Event Listener
+  var secondaryDD= $(".secondaryDD")[0];
+  $(secondaryDD).change(function(e) {
+    selectedId = e.target.value;
+    thirdXHR().then(populateDOMproducts);
+  });
+};
+
+
+///// Function to dynamically populate DOM products
+var populateDOMproducts = function(data) {
+  // console.log("new data", data);
+
+  var myString = '';
+  
+  myString += `<select class="tertiaryDD">`;
+  myString += `<option disabled="true" selected="true" value="0">Select a product</option>`;
+  
+      for (var i = 0; i < data.products.length; i++) {
+        // console.log("selectedId", selectedId)
+        // console.log("each data", data.products[i].type)
+
+        if (selectedId == data.products[i].type) {
+          myString += `<option value="${data.products[i].description}">${data.products[i].name}</option>`;
+        }
+      }
+
+  myString += `</select>`;
+  productsDD.html(myString);
+
+
+// Event Listener
+  var tertiaryDD= $(".tertiaryDD")[0];
+  $(tertiaryDD).change(function(e) {
+    selectedId = e.target.value;
+    populateProductDescription(e);
+  });
+};
+
+
+//// Function to populate DOM with HTML that describes product
+var populateProductDescription = function(e) {
+  console.log("Description:", e.target.value);
+
+  var myString = '';
+
+  myString += `<h2> Product Description </h2>`;
+  myString += `<p>${e.target.value}</p>`;
+  desciptDiv.html(myString); 
+  
 };
 
 
 firstXHR().then(populateDOMcategories);
+
+
+
+// Understanding global variables example from Bill
+// var fun = "this is some fun"
+
+// var myFun = function () {
+//   console.log("fun ", fun)
+// }
+
+// myFun();
+// console.log("fun", fun)
 
 
 
